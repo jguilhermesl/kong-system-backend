4588ed47fdcac3fe12634474d44ffead40a4a725
João Guilherme <joaoguilherme_pe@hotmail.com>
2025-02-08 15:18:51 -0300

    feat: add update financial

--- src/functions/financial /routes.ts
+++ src/functions/financial /routes.ts
@@ -4,11 +4,13 @@ import { isAdminMiddleware } from '@/middlewares/admin-middleware';
 import { fetchFinancial } from './fetch-financial';
 import { addFinancial } from './add-financial';
 import { deleteFinancial } from './delete-financial';
+import { updateFinancial } from './update-financial';
 
 const routerFinancial = Router();
 
 routerFinancial.get('/financial', authMiddleware, isAdminMiddleware, fetchFinancial);
 routerFinancial.post('/financial', authMiddleware, isAdminMiddleware, addFinancial);
 routerFinancial.delete('/financial/:id', authMiddleware, isAdminMiddleware, deleteFinancial);
+routerFinancial.put('/financial/:id', authMiddleware, isAdminMiddleware, updateFinancial);
 
 export { routerFinancial }
---
+++ src/functions/financial /update-financial.ts
@@ -0,0 +1,37 @@
+import { FinancialDAO } from "../../DAO/financial";
+import { handleErrors } from "../../utils/handle-errors";
+import { z } from "zod";
+
+const financialSchema = z.object({
+  productType: z.string().min(1, "Product type is required").optional(),
+  productName: z.string().min(1, "Product name is required").optional(),
+  saleValue: z.number().positive("Sale value must be positive").optional(),
+  paidOrRefunded: z.boolean().optional()
+});
+
+export const updateFinancial = async (req: any, res: any) => {
+  try {
+    const financialDAO = new FinancialDAO();
+    const { id: financialId } = req.params;
+    const parsedData = financialSchema.parse(req.body);
+
+    const updatedItem = await financialDAO.updateOne({
+      id: (itemId) => itemId === financialId
+    }, {
+      productName: parsedData.productName,
+      productType: parsedData.productType,
+      saleValue: parsedData.saleValue || 0,
+      paidOrRefunded: parsedData.paidOrRefunded
+    });
+
+    if (!updatedItem) {
+      return res.status(404).send({ message: "Financial record not found" });
+    }
+
+    return res.status(200).send({ message: "Financial record updated successfully" });
+  } catch (err) {
+    const errorMessage = handleErrors(err);
+
+    return res.status(500).send({ message: errorMessage });
+  }
+};
